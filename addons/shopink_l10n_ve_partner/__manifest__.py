74d7e3df01f388c2f5a2cbbc4c1d82f79ad1f0de
adminshopink <admin.shopink@gmail.com>
2026-07-16 19:56:46 -0400

    Create __manifest__.py

---
+++ addons/shopink_l10n_ve_partner/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Shopink - Contactos Localización VE',
+    'summary': 'Campos de RIF, Cédula y Tipos de Persona para clientes y proveedores.',
+    'version': '19.0.1.0.0',
+    'category': 'Base',
+    'author': 'Shopink',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+        'contacts',
+    ],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
