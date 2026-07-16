a1d58f2bf9e3c0472d5922674ec47d706c793496
adminshopink <admin.shopink@gmail.com>
2026-07-16 06:50:41 -0400

    Add files via upload

---
+++ addons/account_invoice_pricelist_sale/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Account Invoice Pricelist Sale',
+    'version': '19.0.1.0.0',
+    'category': 'Invoicing',
+    'summary': 'Link between account_invoice_pricelist and sale',
+    'author': 'OCA/account-invoicing',
+    'website': 'https://github.com/OCA/account-invoicing',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_invoice_pricelist',
+        'sale',
+    ],
+    'data': [],
+    'installable': True,
+    'auto_install': True,
+}
