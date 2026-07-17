23b91f2b5a80051fd0d650cb67f2a8ad67a5cb29
adminshopink <admin.shopink@gmail.com>
2026-07-17 00:35:57 -0400

    Create __manifest__.py

---
+++ addons/shopink_l10n_ve_sale/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Shopink - Ventas Localización VE',
+    'summary': 'Campos fiscales congelados en Cotizaciones y Órdenes de Venta.',
+    'version': '19.0.1.0.0',
+    'category': 'Sales',
+    'author': 'Shopink',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale',
+        'shopink_l10n_ve_contacts',  # Dependencia clave
+        'web_studio',
+    ],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
