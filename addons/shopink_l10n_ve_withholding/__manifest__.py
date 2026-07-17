bd2973a3d97a859acd63dfa31e2d01469bb17c7d
adminshopink <admin.shopink@gmail.com>
2026-07-16 20:35:02 -0400

    Create __manifest__.py

---
+++ addons/shopink_l10n_ve_withholding/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Shopink - Localización Venezolana (Retenciones IVA e ISLR)',
+    'version': '19.0.1.0.0',
+    'summary': 'Gestión de retenciones de IVA recibidas e ISLR mediante diarios contables.',
+    'category': 'Accounting/Localizations',
+    'author': 'Shopink',
+    'depends': ['account', 'web_studio', 'shopink_l10n_ve_partner'],
+    'data': [], # Vacío para configurar la UI con Studio directamente en producción
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
