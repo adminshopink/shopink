dd05a8489f41fe49d1481f74a90fc1f8da5b19e0
adminshopink <admin.shopink@gmail.com>
2026-07-17 00:46:48 -0400

    Update __manifest__.py

--- addons/shopink_l10n_ve_sale/__manifest__.py
+++ addons/shopink_l10n_ve_sale/__manifest__.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Shopink - Ventas Localización VE',
-    'summary': 'Campos fiscales congelados en Cotizaciones y Órdenes de Venta.',
+    'summary': 'Campos fiscales y reporte de cotización predeterminado para Venezuela.',
     'version': '19.0.1.0.0',
     'category': 'Sales',
     'author': 'Shopink',
     'license': 'LGPL-3',
     'depends': [
         'sale',
-        'shopink_l10n_ve_contacts',  # Dependencia clave
-        'web_studio',
+        'shopink_l10n_ve_contacts',
     ],
-    'data': [],
+    'data': [
+        'report/sale_report_templates.xml',  # <-- Registramos el archivo del reporte
+    ],
     'installable': True,
     'application': False,
     'auto_install': False,
