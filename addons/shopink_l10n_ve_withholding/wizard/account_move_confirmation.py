28fba0e5fa36d38277d884a53ae33fcdd0fc88fb
adminshopink <admin.shopink@gmail.com>
2026-07-17 03:00:31 -0400

    Update account_move_confirmation.py

--- addons/shopink_l10n_ve_withholding/wizard/account_move_confirmation.py
+++ addons/shopink_l10n_ve_withholding/wizard/account_move_confirmation.py
@@ -1,16 +1,15 @@
-from odoo import models, fields, _
-from odoo.exceptions import UserError
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-class AccountMoveConfirmationWizard(models.TransientModel):
+class AccountMoveConfirmationWizard(TransientModel):
     _name = 'account.move.confirmation.wizard'
-    _description = 'Confirmación Crítica de Facturación Digital'
+    _description = 'Asistente de Confirmacion de Factura Digital'
 
-    move_id = fields.Many2one('account.move', string='Factura', required=True)
+    move_id = fields.Many2one('account.move', string='Factura', required=True, ondelete='cascade')
     partner_id = fields.Many2one('res.partner', related='move_id.partner_id', string='Cliente')
-    amount_total = fields.Monetary(related='move_id.amount_total', currency_field='currency_id', string='Total')
-    currency_id = fields.Many2one('res.currency', related='move_id.currency_id')
+    amount_total = fields.Float(related='move_id.amount_total', string='Monto Total')
 
     def action_force_post(self):
         self.ensure_one()
-        # Procedemos a publicar la factura de manera nativa una vez confirmado
+        # Se ejecuta action_post pasando el contexto que salta este mismo wizard
         return self.move_id.with_context(skip_confirmation_wizard=True).action_post()
