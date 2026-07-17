6eb49f8f922a9c57d100b8e7bc7dcf0d44d6a6bd
adminshopink <admin.shopink@gmail.com>
2026-07-17 02:03:14 -0400

    Update __manifest__.py

--- addons/shopink_l10n_ve_invoice_control/__manifest__.py
+++ addons/shopink_l10n_ve_invoice_control/__manifest__.py
@@ -8,6 +8,7 @@
     'depends': ['account', 'web_studio'],
     'data': [
         'data/ir_sequence_data.xml',
+        'views/account_move_views.xml', # <- Agregamos la vista estructural de forma limpia
     ],
     'installable': True,
     'application': False,
