1791ed7d06de40ec7c6b48a26dcc1260d6609f16
adminshopink <admin.shopink@gmail.com>
2026-07-16 21:31:04 -0400

    Create __manifest__.py

---
+++ addons/shopink_l10n_ve_reports/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Shopink - Localización Venezolana (Informes Fiscales y Libros)',
+    'version': '19.0.1.0.0',
+    'summary': 'Generación de Libros de Compras/Ventas en formato compatible con Excel.',
+    'category': 'Accounting/Localizations',
+    'author': 'Shopink',
+    'depends': ['account', 'web_studio', 'shopink_l10n_ve_partner', 'shopink_l10n_ve_withholding'],
+    'data': [], # Mantenemos vacío para configurar el Wizard con Studio en producción
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
