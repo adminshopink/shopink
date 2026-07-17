24a39e7854399103df29a6c9e2aa3b3a96c01a4f
adminshopink <admin.shopink@gmail.com>
2026-07-16 21:51:02 -0400

    Create __manifest__.py

---
+++ addons/shopink_l10n_ve_invoice_control/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Shopink - Localización Venezolana (Control de Facturación)',
+    'version': '19.0.1.0.0',
+    'summary': 'Asignación automática del Número de Control legal para facturas de clientes.',
+    'category': 'Accounting/Localizations',
+    'author': 'Shopink',
+    'depends': ['account', 'web_studio'],
+    'data': [
+        'data/ir_sequence_data.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
