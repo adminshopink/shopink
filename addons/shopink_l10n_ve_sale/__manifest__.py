63b96b0a7a59094f187618db9babf0b9c915b7c0
adminshopink <admin.shopink@gmail.com>
2026-07-17 01:12:14 -0400

    Update __manifest__.py

--- addons/shopink_l10n_ve_sale/__manifest__.py
+++ addons/shopink_l10n_ve_sale/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Shopink - Ventas Localización VE',
-    'summary': 'Campos fiscales y reporte de cotización predeterminado para Venezuela.',
+    'summary': 'Campos fiscales, reporte de cotización para Venezuela y botón de WhatsApp.',
     'version': '19.0.1.0.0',
     'category': 'Sales',
     'author': 'Shopink',
@@ -11,7 +11,8 @@
         'shopink_l10n_ve_partner',
     ],
     'data': [
-        'report/sale_report_templates.xml',  # <-- Registramos el archivo del reporte
+        'report/sale_report_templates.xml',
+        'views/sale_order_views.xml',  # 👈 Aquí le decimos a Odoo que cargue el botón
     ],
     'installable': True,
     'application': False,
